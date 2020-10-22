9b29e34d7d92c3912ba32cb8d2bffa483b022444
zezhongyan <zezhongyan@sohu-inc.com>
2020-10-22 14:26:44 +0800

    提交

--- src/main/java/com/enjoy/service/impl/serviceImpl.java
+++ src/main/java/com/enjoy/service/impl/serviceImpl.java
@@ -5,6 +5,11 @@ import com.enjoy.service.MyService;
 @Service("Myservice")
 public class serviceImpl implements MyService {
     @Override
+    public String query1(String name, String age) {
+        return null;
+    }
+
+    @Override
     public String query(String name, String age) {
         return name+"===="+age;
     }
